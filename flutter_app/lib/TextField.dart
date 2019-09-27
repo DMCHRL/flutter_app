136623d49de87a400b03b487e933bb98505486ec
liang <1529730472@qq.com>
2019-09-27 17:56:51 +0800

    1

--- flutter_app/lib/TextField.dart
+++ flutter_app/lib/TextField.dart
@@ -14,20 +14,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Show(title: "文本框"),
+      home: FocusTestRoute(title: "文本框"),
     );
   }
 }
 
-class Show extends StatelessWidget {
+/**
+ * 文本框
+ */
+class Show extends StatefulWidget {
 
   Show({Key key,@required this.title}):super(key:key);
   final String title;
 
+  @override
+  _ShowState createState() {
+    return _ShowState();
+  }
+}
+
+class _ShowState extends State<Show> {
   TextEditingController _unameController = TextEditingController();
 
   @override
   void initState() {
+    _unameController.text="hello world";
+    _unameController.selection=TextSelection(
+        baseOffset: 2,
+        extentOffset: _unameController.text.length
+    );
     //监听输入改变
     _unameController.addListener((){
       print(_unameController.text);
@@ -38,20 +53,21 @@ class Show extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Column(
         children: <Widget>[
+
           TextField(
             autofocus: true,
-          keyboardType:TextInputType.phone,
             decoration: InputDecoration(
                 labelText: "用户名",
                 hintText: "用户名或邮箱",
                 prefixIcon: Icon(Icons.person)
             ),
             controller: _unameController,
-          /*  onChanged: (V){
+            textInputAction:TextInputAction.search,
+            /*  onChanged: (V){
               print(_unameController.text);
             },*/
           ),
@@ -67,7 +83,72 @@ class Show extends StatelessWidget {
       ),
     );
   }
-
-
 }
 
+/**
+ * 焦点
+ */
+class FocusTestRoute extends StatefulWidget{
+  FocusTestRoute({Key key,@required this.title}):super(key:key);
+  final String title;
+
+  @override
+  _FocusTestRouteState createState() {
+    return _FocusTestRouteState();
+  }
+}
+
+class _FocusTestRouteState extends State<FocusTestRoute>{
+  FocusNode focusNode1 = new FocusNode();
+  FocusNode focusNode2 = new FocusNode();
+  FocusScopeNode focusScopeNode;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            autofocus: true,
+             focusNode: focusNode1,
+            decoration: InputDecoration(
+                labelText: "input1"
+            ),
+          ),
+          TextField(
+             focusNode: focusNode2,//关联focusNode2
+            decoration: InputDecoration(
+                labelText: "input2"
+            ),
+          ),
+          Builder(builder:(ctx){
+            return Column(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("移动焦点"),
+                  onPressed: (){
+                    if(null == focusScopeNode){
+                      focusScopeNode = FocusScope.of(context);
+                    }
+                    focusScopeNode.requestFocus(focusNode2);
+                  },
+                ),
+                RaisedButton(
+                  child: Text("隐藏键盘"),
+                  onPressed: () {
+                    // 当所有编辑框都失去焦点时键盘就会收起
+                    focusNode1.unfocus();
+                    focusNode2.unfocus();
+                  },
+                ),
+              ],
+            );
+          })
+        ],
+      ),
+    );
+  }
+}
