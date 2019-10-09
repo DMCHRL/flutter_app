897a4b9c15af5e9875093beec32e4164fa2a8c04
DMCHRL <1529730472@qq.com>
2019-10-09 23:36:55 +0800

    10/8 commit

--- flutter_app/lib/component/GradientButton.dart
+++ flutter_app/lib/component/GradientButton.dart
@@ -14,13 +14,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: WillPopScopeRoute(),
+      home: GradientButtonRoute(),
     );
   }
 }
 
 
-/*class GradientButton extends StatelessWidget{
+class GradientButton extends StatelessWidget{
   // 渐变色数组
   final List<Color> colors;
 
@@ -44,8 +44,77 @@ class MyApp extends StatelessWidget {
 });
 
   Widget build(BuildContext context){
-    return null;
+    ThemeData theme = Theme.of(context);
+
+    //确保colors数组不空
+    List<Color> _colors = colors ?? [theme.primaryColor,theme.primaryColorDark?? theme.primaryColor];
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: _colors),
+        borderRadius:  borderRadius,
+      ),
+      child: Material(
+        type: MaterialType.transparency,
+        child: InkWell(
+          splashColor: _colors.last,
+          highlightColor: Colors.transparent,
+          borderRadius: borderRadius,
+          onTap: onPressed,
+          child: ConstrainedBox(constraints: BoxConstraints.tightFor(height: height,width: width),
+          child: Center(
+            child: Padding(padding: const EdgeInsets.all(8.0),
+              child: DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: child),
+            ),
+          ),
+          ),
+        ),
+      ),
+    );
   }
 
-}*/
+}
+
+class GradientButtonRoute extends StatefulWidget{
+
+  @override
+  _GradientButtonRouteState createState() {
+    return _GradientButtonRouteState();
+  }
+}
+
+class _GradientButtonRouteState extends State<GradientButtonRoute>{
+  Widget build(BuildContext context){
+    return  Material(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            GradientButton(
+              colors: [Colors.orange,Colors.red],
+              height: 50,
+              child: Text("Submit"),
+              onPressed: onTap,
+            ),
+            GradientButton(
+
+              height: 50,
+              child: Text("Submit"),
+              onPressed: onTap,
+            ),
+            GradientButton(
+              colors: [Colors.lightBlue[300],Colors.green],
+              height: 50,
+              child: Text("Submit"),
+              onPressed: onTap,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  onTap(){
+    print("button click");
+  }
+}
 
